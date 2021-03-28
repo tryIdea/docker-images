bf8233a7addcce0bd78ad675b63824fd7941970f
dufugang <dufugang@yunlizhihui.com>
2021-03-28 09:02:10 +0800

    Initial commit

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ tmp_image=${image#*\/}
 gen_image=${tmp_image//[\/:]/-}
 
 echo "will download this image from: registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}"
-docker pull registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}
-docker tag registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image} ${image}
-docker rmi registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}
+docker pull registry.cn-beijing.aliyuncs.com/croot/docker-images:${gen_image}
+docker tag registry.cn-beijing.aliyuncs.com/croot/docker-images:${gen_image} ${image}
+docker rmi registry.cn-beijing.aliyuncs.com/croot/docker-images:${gen_image}
 echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> download successfully!\n"
